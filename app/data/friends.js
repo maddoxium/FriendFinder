25c77358a57113221d860a8c2e390a1b93eea56b
matt babcock <babcockmatthew12@yahoo.com>
2018-07-16 17:30:31 -0700

    final commit

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+      name: "Blige Rat Steve",
+      photo: "https://imgur.com/xYKf9c9",
+      scores: [
+        "3",
+        "4",
+        "2",
+        "4",
+        "4",
+        "1",
+        "5",
+        "2",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Cat Man",
+      photo: "https://imgur.com/rEhZ7Tw",
+      scores: [
+        "5",
+        "2",
+        "5",
+        "4",
+        "3",
+        "3",
+        "2",
+        "3",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Spell Casty Boi",
+      photo: "https://imgur.com/AA6fU8Z",
+      scores: [
+        "2",
+        "3",
+        "4",
+        "5",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "1"
+      ]
+    },
+    {
+      name: "Aesthetic Girl",
+      photo: "https://imgur.com/c9rZjG5",
+      scores: [
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3"
+      ]
+    },
+    {
+      name: "Helmety Lad",
+      photo: "https://imgur.com/9FJPyRe",
+      scores: [
+        "4",
+        "4",
+        "4",
+        "4",
+        "5",
+        "2",
+        "4",
+        "3",
+        "4",
+        "4"
+      ]
+    },
+    {
+      name: "Peppy Lass",
+      photo: "https://imgur.com/imf4XFp",
+      scores: [
+        "2",
+        "4",
+        "2",
+        "3",
+        "1",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  module.exports = friends;
+  
